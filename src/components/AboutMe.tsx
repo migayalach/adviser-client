fde3ec2ebc71836adedf3f38669804afda2a7d5a
migayalach <ayalachavezmiguel@gmail.com>
2025-08-21 12:41:03 -0400

    added info

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function AboutMe() {
   return (
-    <div>AboutMe</div>
-  )
+    <div>
+      <h1>Conoce a Stephani</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipiscing elit cubilia, libero
+        convallis nam fringilla cursus primis suscipit dictum, nibh urna taciti
+        tempus ac ultrices interdum. Tempor semper lacinia quisque cum justo
+        euismod sem aliquam facilisis, platea ad pretium venenatis rhoncus quis
+        porttitor malesuada mus, vehicula nullam vulputate augue vitae lacus
+        iaculis luctus. Orci sollicitudin ac ligula eros venenatis feugiat
+        parturient libero magna, semper donec pellentesque pretium porta
+        hendrerit maecenas auctor aptent, tortor sed viverra placerat cursus vel
+        quisque quis.
+      </p>
+    </div>
+  );
 }
 
-export default AboutMe
+export default AboutMe;
