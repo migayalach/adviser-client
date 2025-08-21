34d3d14e4c2f81d228de5b5c08035e9d98c546e8
migayalach <ayalachavezmiguel@gmail.com>
2025-08-21 11:54:23 -0400

    import components and export

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import Profile from "./Profile";
+import AboutMe from "./AboutMe";
+import Contact from "./Contact";
+import Courses from "./Courses";
+import FeedBack from "./FeedBack";
+import Footer from "./Footer";
+import Services from "./Services";
+
+export { Profile, AboutMe, Contact, Courses, FeedBack, Footer, Services };
