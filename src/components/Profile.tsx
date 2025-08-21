8e14e279f019f21eebe52b919469ca083199fba4
migayalach <ayalachavezmiguel@gmail.com>
2025-08-21 12:41:35 -0400

    added info

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Card, CardHeader, CardBody } from "@heroui/card";
+import { Image } from "@heroui/image";
 
 function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <Card className="py-0">
+      <CardBody className="overflow-visible py-2">
+        <Image
+          alt="Card background"
+          className="object-cover rounded-xl"
+          src="https://heroui.com/images/hero-card-complete.jpeg"
+          width={270}
+        />
+      </CardBody>
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <p className="text-tiny uppercase font-bold">Daily Mix</p>
+        <small className="text-default-500">12 Tracks</small>
+        <h4 className="font-bold text-large">Frontend Radio</h4>
+      </CardHeader>
+    </Card>
+  );
 }
 
-export default Profile
+export default Profile;
