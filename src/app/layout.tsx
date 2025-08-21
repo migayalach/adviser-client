1cc74fa6901ec846bdc5ec98076ba66c2a628365
migayalach <ayalachavezmiguel@gmail.com>
2025-08-21 11:58:14 -0400

    import component footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import { Metadata, Viewport } from "next";
-import { Link } from "@heroui/link";
 import clsx from "clsx";
 import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/src/components/navbar";
 import "@/src/styles/globals.css";
+import { Footer } from "@/src/components";
 
 export const metadata: Metadata = {
   title: {
@@ -46,15 +46,7 @@ export default function RootLayout({
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
-              </Link>
+              <Footer />
             </footer>
           </div>
         </Providers>
