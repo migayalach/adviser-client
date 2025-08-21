d26b25fefa1438448f7853e30700c9e70cb7e89d
migayalach <ayalachavezmiguel@gmail.com>
2025-08-21 12:41:44 -0400

    added info

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { Card, CardHeader, CardBody } from "@heroui/card";
+import { Image } from "@heroui/image";
+import React from "react";
 
 function Services() {
   return (
-    <div>Services</div>
-  )
+    <div>
+      <h1>Servicios y Asesorias</h1>
+      <Card className="py-4">
+        <CardBody className="overflow-visible py-2">
+          <Image
+            alt="Card background"
+            className="object-cover rounded-xl"
+            src="https://heroui.com/images/hero-card-complete.jpeg"
+            width={270}
+          />
+        </CardBody>
+        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+          <p className="text-tiny uppercase font-bold">Daily Mix</p>
+          <small className="text-default-500">12 Tracks</small>
+          <h4 className="font-bold text-large">Frontend Radio</h4>
+        </CardHeader>
+      </Card>
+      <Card className="py-4">
+        <CardBody className="overflow-visible py-2">
+          <Image
+            alt="Card background"
+            className="object-cover rounded-xl"
+            src="https://heroui.com/images/hero-card-complete.jpeg"
+            width={270}
+          />
+        </CardBody>
+        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+          <p className="text-tiny uppercase font-bold">Daily Mix</p>
+          <small className="text-default-500">12 Tracks</small>
+          <h4 className="font-bold text-large">Frontend Radio</h4>
+        </CardHeader>
+      </Card>
+      <Card className="py-4">
+        <CardBody className="overflow-visible py-2">
+          <Image
+            alt="Card background"
+            className="object-cover rounded-xl"
+            src="https://heroui.com/images/hero-card-complete.jpeg"
+            width={270}
+          />
+        </CardBody>
+        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+          <p className="text-tiny uppercase font-bold">Daily Mix</p>
+          <small className="text-default-500">12 Tracks</small>
+          <h4 className="font-bold text-large">Frontend Radio</h4>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 }
 
-export default Services
+export default Services;
