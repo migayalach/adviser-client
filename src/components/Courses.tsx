851a63b3c2e471562de5d6430c13a6d20f9f4989
migayalach <ayalachavezmiguel@gmail.com>
2025-08-21 12:41:12 -0400

    added info

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Image } from "@heroui/image";
+import React from "react";
 
 function Courses() {
   return (
-    <div>Courses</div>
-  )
+    <div>
+      <h1>Cursos</h1>
+      <Image
+        alt="Card background"
+        className="object-cover rounded-xl"
+        src="https://heroui.com/images/hero-card-complete.jpeg"
+        width={270}
+      />
+    </div>
+  );
 }
 
-export default Courses
+export default Courses;
