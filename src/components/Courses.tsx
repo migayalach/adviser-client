e53bc838c881a012f1113c9576b5cd0f383fc86e
migayalach <ayalachavezmiguel@gmail.com>
2025-08-22 13:08:00 -0400

    added info

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -1,16 +1,27 @@
 import { Image } from "@heroui/image";
 import React from "react";
+import { Cards, Buttons } from "@/src/components";
+
+const courses = [
+  {
+    id: "1C",
+    title: "Binance Desde Cero",
+    image:
+      "https://res.cloudinary.com/dqgcyonb9/image/upload/v1755873891/adviser/qopth7xbtcccblghjksp.jpg",
+  },
+];
 
 function Courses() {
   return (
     <div>
       <h1>Cursos</h1>
-      <Image
-        alt="Card background"
-        className="object-cover rounded-xl"
-        src="https://heroui.com/images/hero-card-complete.jpeg"
-        width={270}
-      />
+      {courses.map(({ id, title, image }, index) => {
+        return (
+          <div key={index}>
+            <Cards id={id} title={title} image={image} />
+          </div>
+        );
+      })}
     </div>
   );
 }
