3c87f7f1fa57471df8e77fdf90eced47e3df4c9b
migayalach <ayalachavezmiguel@gmail.com>
2025-08-22 13:07:50 -0400

    added info

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <h1>Hablemos</h1>
+      <p>
+        Contáctame para iniciar tu proyecto de desarrollo web y hacer realidad
+        tu visión.
+      </p>
+      <button>Contacto</button>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
