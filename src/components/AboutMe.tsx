4b52e6c1ba600efbfd5000bc4adc69459c4696e3
migayalach <ayalachavezmiguel@gmail.com>
2025-08-22 13:07:38 -0400

    added info

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -5,15 +5,9 @@ function AboutMe() {
     <div>
       <h1>Conoce a Stephani</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit cubilia, libero
-        convallis nam fringilla cursus primis suscipit dictum, nibh urna taciti
-        tempus ac ultrices interdum. Tempor semper lacinia quisque cum justo
-        euismod sem aliquam facilisis, platea ad pretium venenatis rhoncus quis
-        porttitor malesuada mus, vehicula nullam vulputate augue vitae lacus
-        iaculis luctus. Orci sollicitudin ac ligula eros venenatis feugiat
-        parturient libero magna, semper donec pellentesque pretium porta
-        hendrerit maecenas auctor aptent, tortor sed viverra placerat cursus vel
-        quisque quis.
+        Soy una profesional especialista en Ingeniería, Economía y Finanzas.
+        Cuento con una amplia experiencia en el sector bancario y financiero, y
+        hoy quiero compartir todos mis conocimientos con ustedes!
       </p>
     </div>
   );
