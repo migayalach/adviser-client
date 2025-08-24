c334ad815920095dc3f5f6a863e7ef85e9e61950
migayalach <ayalachavezmiguel@gmail.com>
2025-08-23 20:35:33 -0400

    added new styles

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,35 +1,17 @@
 import React from "react";
 import { Cards } from "@/src/components";
-
-const services = [
-  {
-    id: "1S",
-    title: "Planification financiera",
-    image:
-      "https://res.cloudinary.com/dqgcyonb9/image/upload/v1755872105/adviser/dnqcnhfofwsleh8x2eei.png",
-  },
-  {
-    id: "2S",
-    title: "Inversion Segura",
-    image:
-      "https://res.cloudinary.com/dqgcyonb9/image/upload/v1755872102/adviser/yxhivnlijjmpw85rzllb.jpg",
-  },
-  {
-    id: "3S",
-    title: "Educacion Financiera",
-    image:
-      "https://res.cloudinary.com/dqgcyonb9/image/upload/v1755872108/adviser/ax80l8p3z5k2kyumn3v7.jpg",
-  },
-];
+import { services } from "@/src/mocks";
 
 function Services() {
   return (
-    <div>
-      <h1>Servicios y Asesorias</h1>
-      {services.map(({ id, title, image }, index) => {
+    <div className="bg-[#fff] p-5">
+      <h1 className="text-[24px] text-center font-semibold mb-2">
+        Servicios y Asesorias
+      </h1>
+      {services?.map(({ id, title, image }, index) => {
         return (
           <div key={index}>
-            <Cards id={id} title={title} image={image} />
+            <Cards id={id} title={title} image={image} path="services" />
           </div>
         );
       })}
