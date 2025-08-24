9d5f6007b580523ce2b9974ede220f146492cc67
migayalach <ayalachavezmiguel@gmail.com>
2025-08-23 20:36:39 -0400

    added new styles

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -7,23 +7,35 @@ interface InputCards {
   id: string;
   title: string;
   image: string;
+  path: "courses" | "services";
 }
 
-function Cards({ id, title, image }: InputCards) {
+
+function Cards({ id, title, image, path }: InputCards) {
   return (
-    <Card className="py-4">
+    <Card
+      className={`${path === "courses" ? "bg-[#111827]" : "bg-[#fff] py-4"} mt-4`}
+    >
       <CardBody className="overflow-visible py-2">
         <Image
           alt={title}
           className="object-cover rounded-xl"
           src={image}
-          width={270}
+          width="400px"
+          height={path === "services" ? "220px" : "auto"}
         />
       </CardBody>
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <h4 className="font-bold text-large">{title}</h4>
-      </CardHeader>
-      <Buttons id={id} />
+      {/* TODO - ARREGLAR BUTTON */}
+      <div className="flex flex-col justify-center items-center">
+        <CardHeader className="pb-0 py-2 px-4 flex-col items-center">
+          <h2
+            className={`${path === "courses" ? "text-[#fff]" : "text-[#000]"} "font-bold text-large text-center text-[22px]"`}
+          >
+            {title}
+          </h2>
+        </CardHeader>
+        <Buttons id={id} />
+      </div>
     </Card>
   );
 }
