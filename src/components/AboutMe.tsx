146b3e78c1c2226a6955197a9aaf64f80a2e7959
migayalach <ayalachavezmiguel@gmail.com>
2025-08-23 20:35:45 -0400

    added new styles

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function AboutMe() {
   return (
-    <div>
-      <h1>Conoce a Stephani</h1>
-      <p>
+    <div className="bg-[#111827] text-[#fff] p-5 text-center">
+      <h1 className="text-[22px] mb-2 font-semibold">Conoce a Stephani</h1>
+      <p className="text-[16px]">
         Soy una profesional especialista en Ingeniería, Economía y Finanzas.
         Cuento con una amplia experiencia en el sector bancario y financiero, y
         hoy quiero compartir todos mis conocimientos con ustedes!
