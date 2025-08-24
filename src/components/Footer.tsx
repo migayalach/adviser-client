d2486b8fddd75fe8f183a4d581451afcb84c15b7
migayalach <ayalachavezmiguel@gmail.com>
2025-08-23 20:35:59 -0400

    added new styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,13 @@
-import { Link } from "@heroui/link";
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="flex items-center justify-center text-center bg-[#111827] h-8 p-4">
+      <span className="text-[#fff] text-[12px]">
+        Creado por Miguel Ayala Chávez (2025) ©
+      </span>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
