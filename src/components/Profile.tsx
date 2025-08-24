019670788e4d2bf67831ffaa987f5f5e50fd5b60
migayalach <ayalachavezmiguel@gmail.com>
2025-08-23 20:35:17 -0400

    added new styles

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,18 +4,20 @@ import { Image } from "@heroui/image";
 
 function Profile() {
   return (
-    <Card className="py-0">
+    <Card className="bg-[#fff] p-4">
       <CardBody className="overflow-visible py-2">
         <Image
           alt="Card background"
           className="object-cover rounded-xl"
           src="https://res.cloudinary.com/dqgcyonb9/image/upload/v1755834589/adviser/q2ucn06zvmhyjd0cjb30.png"
-          width={270}
+          width="auto"
         />
       </CardBody>
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <h1 className="font-bold text-large">Stephani Bustillos</h1>
-        <p className="text-tiny font-bold">Asesor Financiero</p>
+        <div className="flex flex-col items-center justify-center">
+        <h1 className="font-bold text-large text-[33px] mt-2">Stephani Bustillos</h1>
+        <p className="text-tiny font-bold text-[19px] mt-6">Asesor Financiero</p>
+        </div>
       </CardHeader>
     </Card>
   );
