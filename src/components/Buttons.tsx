e23eccf35cc71a4f1470ce7fbaa0cb5fab773801
migayalach <ayalachavezmiguel@gmail.com>
2025-08-25 04:16:42 -0400

    added new styles

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Button } from "@heroui/button";
 import Link from "next/link";
+import "@/src/styles/text.css";
 
 function Buttons({ id }: { id: string }) {
   return (
-    <Button color="primary">
+    <Button className="text-[18px] button-color-services-light">
       <Link href={`/${id}`}>Saber más</Link>
     </Button>
   );
