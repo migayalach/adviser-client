4904c34eb93108438fc82966fbef7a85ab9914a6
migayalach <ayalachavezmiguel@gmail.com>
2025-08-25 04:14:40 -0400

    added new styles

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Cards } from "@/src/components";
 import { services } from "@/src/mocks";
+import "@/src/styles/text.css";
 
 function Services() {
   return (
-    <div className="bg-[#fff] p-5">
-      <h1 className="text-[24px] text-center font-semibold mb-2">
+    <div className="container-one-light p-5">
+      <h1 className="text-[27px] text-center font-semibold mb-2">
         Servicios y Asesorias
       </h1>
       {services?.map(({ id, title, image }, index) => {
         return (
           <div key={index}>
-            <Cards id={id} title={title} image={image} path="services" />
+            <Cards id={id} title={title} image={image} />
           </div>
         );
       })}
