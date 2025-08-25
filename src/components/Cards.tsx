c699628d10459be19f8413b648a2abc947e4f16b
migayalach <ayalachavezmiguel@gmail.com>
2025-08-25 04:14:23 -0400

    added new styles

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -2,40 +2,33 @@ import React from "react";
 import { Card, CardHeader, CardBody } from "@heroui/card";
 import { Image } from "@heroui/image";
 import { Buttons } from "@/src/components";
+import "@/src/styles/text.css";
 
 interface InputCards {
   id: string;
   title: string;
   image: string;
-  path: "courses" | "services";
 }
 
-
-function Cards({ id, title, image, path }: InputCards) {
+function Cards({ id, title, image }: InputCards) {
   return (
-    <Card
-      className={`${path === "courses" ? "bg-[#111827]" : "bg-[#fff] py-4"} mt-4`}
-    >
+    <Card className="cart-light mt-4 py-2">
       <CardBody className="overflow-visible py-2">
         <Image
           alt={title}
           className="object-cover rounded-xl"
           src={image}
           width="400px"
-          height={path === "services" ? "220px" : "auto"}
+          height="220px"
         />
       </CardBody>
       {/* TODO - ARREGLAR BUTTON */}
-      <div className="flex flex-col justify-center items-center">
-        <CardHeader className="pb-0 py-2 px-4 flex-col items-center">
-          <h2
-            className={`${path === "courses" ? "text-[#fff]" : "text-[#000]"} "font-bold text-large text-center text-[22px]"`}
-          >
-            {title}
-          </h2>
-        </CardHeader>
+      <CardHeader className="pb-0 py-2 px-4 flex-col items-center">
+        <h2 className="text-family-second text-color-light font-medium text-[20px] mb-2">
+          {title}
+        </h2>
         <Buttons id={id} />
-      </div>
+      </CardHeader>
     </Card>
   );
 }
