b30acd03b72667aa47bd5da19420cdb3b2dca2e7
migayalach <ayalachavezmiguel@gmail.com>
2025-08-25 04:15:01 -0400

    added new styles

--- src/components/Networks.tsx
+++ src/components/Networks.tsx
@@ -1,54 +1,55 @@
 import { Link } from "@heroui/link";
 import React from "react";
 import Contact from "./Contact";
+import "@/src/styles/text.css";
 
 function Networks() {
   return (
-    <div className="bg-[#111827] p-4 text-center">
+    <div className="container-two-light text-family px-4 text-center">
       <Contact />
-      <h1 className="text-[#fff] text-[24px] font-semibold">
-        Da el primer paso hacia tus metas financieras
-      </h1>
-      <div className="flex flex-row justify-evenly items-center py-6">
-        <button>
-          <i className="text-[#fff]">
+      <div className="flex flex-col justify-center mt-[-50px] mb-3">
+        <div>
+          <h1 className="text-[27px] font-semibold">
+            Da el primer paso hacia tus metas financieras
+          </h1>
+        </div>
+        <div className="flex flex-row justify-evenly my-6">
+          <button>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
+              width="20"
+              height="20"
               fill="currentColor"
               viewBox="0 0 16 16"
             >
               <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
             </svg>
-          </i>
-        </button>
-        <button>
-          <i className="text-[#fff]">
+          </button>
+
+          <button>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
+              width="20"
+              height="20"
               fill="currentColor"
               viewBox="0 0 16 16"
             >
               <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
             </svg>
-          </i>
-        </button>
-        <button>
-          <i className="text-[#fff]">
+          </button>
+
+          <button>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
+              width="20"
+              height="20"
               fill="currentColor"
               viewBox="0 0 16 16"
             >
               <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
             </svg>
-          </i>
-        </button>
+          </button>
+        </div>
       </div>
     </div>
   );
