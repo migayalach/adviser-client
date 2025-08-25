874dd8c4ed57f4992b626be0b996b908afaa04b3
migayalach <ayalachavezmiguel@gmail.com>
2025-08-25 04:12:41 -0400

    update metadata

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,9 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Stephani Bustillos",
+  description:
+    "Asesoría financiera personalizada para ayudarte a organizar, invertir y hacer crecer tu patrimonio con seguridad y confianza.",
   navItems: [
     {
       label: "Home",
