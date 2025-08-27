41f9543ea755f408e76b7ee81f1003a42c1497a1
migayalach <ayalachavezmiguel@gmail.com>
2025-08-27 11:54:04 -0400

    added info

--- src/mocks/courses.ts
+++ src/mocks/courses.ts
@@ -4,5 +4,58 @@ export const courses = [
     title: "Binance Desde Cero",
     image:
       "https://res.cloudinary.com/dqgcyonb9/image/upload/v1755873891/adviser/qopth7xbtcccblghjksp.jpg",
+    about: {
+      title:
+        "Curso Básico de Binance - Aprende a Proteger tu Dinero desde Bolivia",
+      subTitle:
+        "Resguarda tu poder adquisitivo ahorrando en dólares digitales (USDT), sin bancos ni fronteras.",
+      from: "Ideal para principiantes sin experiencia previa.",
+      mode: "Modalidad: 100% Virtual (vía Zoom)",
+      hours: {
+        title: "Horario",
+        hours: "19:30 a 21:00",
+      },
+      start: "Inicio: Lunes 04 de agosto de 2025",
+      cost: "Inversión: Bs. 150.-",
+      learn: [
+        {
+          day: "Día 1 - Introducción a las Criptomonedas",
+          items: [
+            "¿Qué es el dinero digital?",
+            "¿Por qué el boliviano pierde valor cada año?",
+            "¿Qué es el USDT y cómo puede ayudarte a proteger tus ahorros?",
+          ],
+        },
+        {
+          day: "Día 2 - Usa Binance con Seguridad",
+          items: [
+            "Crea tu cuenta paso a paso",
+            "Activa la verificación en dos pasos (2FA)",
+            "Aprende a usar Binance desde tu celular",
+          ],
+        },
+        {
+          day: "Día 3 - Compra y Transfiere USDT",
+          items: [
+            "Cómo comprar USDT en Binance P2P desde Bolivia",
+            "Métodos de pago disponibles y seguros",
+            "Transfiere tus fondos a diferentes billeteras",
+          ],
+        },
+        {
+          day: "Día 4 - Ahorra e Invierte de Forma Segura",
+          items: [
+            " Usa el “chanchito” de Binance (Earn) para generar intereses",
+            "Diferencias entre billeteras Spot, Fondos y Billeteras Frías",
+            "Cómo proteger tus activos con seguridad",
+          ],
+        },
+      ],
+      include: [
+        "Incluye ejercicios prácticos todos los días",
+        "Curso pensado para personas que buscan empezar a ahorrar o invertir con bajo riesgo, sin complicaciones técnicas.",
+      ],
+      ask: "¿Estás lista/o para tomar el control de tu dinero?",
+    },
   },
 ];
