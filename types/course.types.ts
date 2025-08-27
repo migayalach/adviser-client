ea2db9c71d9a79f0c8efd77a08c3195cf0d6af31
migayalach <ayalachavezmiguel@gmail.com>
2025-08-27 11:53:21 -0400

    created new types to course data

---
+++ types/course.types.ts
@@ -0,0 +1,23 @@
+// export interface Course {
+//   id: string;
+//   title: string;
+//   image: string;
+//   about: {
+//     title: string;
+//     subTitle: string;
+//     from: string;
+//     mode: string;
+//     hours: {
+//       title: string;
+//       hours: string;
+//     };
+//     start: string;
+//     cost: string;
+//     learn: {
+//       [key: string]: string;
+//       items: string[];
+//     }[];
+//     include: string[];
+//     ask: string;
+//   };
+// }
