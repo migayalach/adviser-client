2b433f8a69c44ee80b77a93c2d6588323ea272be
migayalach <ayalachavezmiguel@gmail.com>
2025-08-30 22:55:10 -0400

    added styles

--- src/app/service/[idService]/page.tsx
+++ src/app/service/[idService]/page.tsx
@@ -39,17 +39,22 @@ function page({ params }: Props) {
   }, [idService]);
 
   return (
-    <div className="cart-light text-color-light flex flex-col justify-center items-center px-7 pt-4 pb-7">
-      <h1 className="text-family mb-3 text-[24px] font-semibold">
+    <div className="cart-light text-color-light flex flex-col justify-center items-center px-7 pt-4 pb-7 md:flex md:flex-col md:min-h-[100vh]">
+      <h1 className="text-family mb-3 text-[24px] font-semibold md:text-[40px] md:mt-[-80px]">
         {info.title}
       </h1>
-      {info?.image && (
-        <Picture alt={info?.title} src={info.image} width="auto" />
-      )}
-      <div className="acordion-light mt-4 p-4 rounded-lg">
-        <p className="text-family-second text-[16px] text-center leading-7">
-          {info.about}
-        </p>
+
+      <div className="md:flex md:flex-row md:justify-center md:items-center">
+        <div className="md:w-[550px]">
+          {info?.image && (
+            <Picture alt={info?.title} src={info.image} width="auto" />
+          )}
+        </div>
+        <div className="acordion-light mt-4 p-4 rounded-lg md:w-[700px] md:ml-10">
+          <p className="text-family-second text-[16px] text-center leading-7 md:text-[22px] md:leading-10">
+            {info.about}
+          </p>
+        </div>
       </div>
     </div>
   );
