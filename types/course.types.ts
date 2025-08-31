cbd34694cda8e54437a57eb46b3b3558c89e47ba
migayalach <ayalachavezmiguel@gmail.com>
2025-08-31 01:10:18 -0400

    created interface to course data

--- types/course.types.ts
+++ types/course.types.ts
@@ -1,23 +1,29 @@
-// export interface Course {
-//   id: string;
-//   title: string;
-//   image: string;
-//   about: {
-//     title: string;
-//     subTitle: string;
-//     from: string;
-//     mode: string;
-//     hours: {
-//       title: string;
-//       hours: string;
-//     };
-//     start: string;
-//     cost: string;
-//     learn: {
-//       [key: string]: string;
-//       items: string[];
-//     }[];
-//     include: string[];
-//     ask: string;
-//   };
-// }
+interface CourseHour {
+  title: string;
+  hours: string;
+}
+
+interface LearnItem {
+  day: string;
+  items: string[];
+}
+
+interface AboutCourse {
+  title: string;
+  subTitle: string;
+  from: string;
+  mode: string;
+  hours: CourseHour;
+  start: string;
+  cost: string;
+  learn: LearnItem[];
+  include: string[];
+  ask: string;
+}
+
+export interface ICourse {
+  id: string;
+  title: string;
+  image: string;
+  about: AboutCourse;
+}
