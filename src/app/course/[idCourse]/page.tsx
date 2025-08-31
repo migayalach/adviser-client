ebf68b5058a2868d890c028625e6f21358ba0c92
migayalach <ayalachavezmiguel@gmail.com>
2025-08-30 23:14:05 -0400

    created component loading and import at course

--- src/app/course/[idCourse]/page.tsx
+++ src/app/course/[idCourse]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { use, useEffect, useState } from "react";
-import { Picture } from "@/src/components";
+import { Picture, Loading } from "@/src/components";
 import { courses } from "@/src/mocks";
 import "@/src/styles/text.css";
 
@@ -20,7 +20,7 @@ function page({ params }: Props) {
     }
   }, [idCourse]);
 
-  if (!data) return <h1>Loading....</h1>;
+  if (!data) return <Loading />;
 
   return (
     <div className="cart-light text-color-light flex flex-col justify-center p-4">
@@ -32,12 +32,16 @@ function page({ params }: Props) {
       </h3>
       <div className="text-family-second text-[15px] flex flex-col justify-center items-center leading-6 md:text-[20px] md:leading-9">
         <h3>{data.about.from}</h3>
-        <p className="font-bold text-[16px] md:text-[20px]">{data.about.mode}</p>
+        <p className="font-bold text-[16px] md:text-[20px]">
+          {data.about.mode}
+        </p>
         <p>
           {data.about.hours.title}: <span>{data.about.hours.hours}</span>
         </p>
         <p>{data.about.start}</p>
-        <p className="font-bold text-[16px] md:text-[20px]">{data.about.cost}</p>
+        <p className="font-bold text-[16px] md:text-[20px]">
+          {data.about.cost}
+        </p>
       </div>
       <div className="text-family-second">
         <div className="md:flex md:flex-row md:justify-center md:items-center">
@@ -47,7 +51,9 @@ function page({ params }: Props) {
           <div className="acordion-light p-2 rounded-2xl md:ml-8 md:p-3 md:w-[650px]">
             {data.about.learn.map((itemDay: any, index: number) => (
               <div key={index} className="mt-3">
-                <h4 className="font-semibold text-[16px] md:text-[20px]">{itemDay.day}</h4>
+                <h4 className="font-semibold text-[16px] md:text-[20px]">
+                  {itemDay.day}
+                </h4>
                 <ol className="p-2 text-[15px] leading-7">
                   {itemDay.items.map((item: any, index: number) => (
                     <li key={index} className="md:text-[18px]">
@@ -67,7 +73,9 @@ function page({ params }: Props) {
               </div>
             );
           })}
-          <p className="text-family text-[22px] md:text-[24px] font-bold">{data.about.ask}</p>
+          <p className="text-family text-[22px] md:text-[24px] font-bold">
+            {data.about.ask}
+          </p>
         </div>
       </div>
     </div>
