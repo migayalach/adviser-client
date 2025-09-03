089d68f2e26e83e1c4601326c5d45f79735fbaf0
migayalach <ayalachavezmiguel@gmail.com>
2025-09-02 23:44:56 -0400

    added transform

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,22 @@ const createJestConfig = nextJest({
   dir: "./",
 });
 
+/** @type {import('jest').Config} */
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
 };
 
 module.exports = createJestConfig(customJestConfig);
